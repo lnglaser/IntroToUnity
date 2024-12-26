98fbc9c481c6e13587e03f9a2d1611fb8f991286
Leon Glaser <leon.glaser@gmail.com>
2024-12-25 23:57:05 -0500

    Test conditionals 2

--- Assets/ConditionalScript.cs
+++ Assets/ConditionalScript.cs
@@ -7,12 +7,30 @@ public class ConditionalScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int score = 10;
+        int score = 3;
 
         if(score == 10)
         {
             Debug.Log("Player has won!");
         }
+
+        if(score !=10)
+        {
+            Debug.Log("Player does not have 10 points.");
+        }
+
+        int health = 50;
+
+        if(health <= 0)
+        {
+            Debug.Log("Player has been destroyed.");
+        }
+
+        if (health > 0) 
+        {
+            Debug.Log("Player still has health.");
+        }
+
     }
 
     // Update is called once per frame
